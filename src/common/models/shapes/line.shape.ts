245942dc545dfc9d72b7f591b8fdf13907feeab3
Reese <reese@singenuity.com>
2021-12-05 11:36:18 -0600

    fixed drag issue with line shape

--- src/common/models/shapes/line.shape.ts
+++ src/common/models/shapes/line.shape.ts
@@ -9,11 +9,13 @@ export default class Line extends BaseShape implements IShape {
   type = ShapeInput.LINE;
   position: ShapePosition;
   origin: { x: number; y: number };
+  origin2: { x: number; y: number }; // track second
 
   constructor(shapeData: ShapeData) {
     super(shapeData);
     this.position = shapeData.position as ShapePosition;
     this.origin = { x: this.position.x1, y: this.position.y1 };
+    this.origin2 = { x: this.position.x2, y: this.position.y2 };
   }
 
   get bounds(): ShapePosition {
@@ -28,9 +30,12 @@ export default class Line extends BaseShape implements IShape {
   dragTo(pos: { x: number; y: number }) {
     this.position.x1 = this.origin.x + pos.x;
     this.position.y1 = this.origin.y + pos.y;
+    this.position.x2 = this.origin2.x + pos.x;
+    this.position.y2 = this.origin2.y + pos.y;
   }
 
   endDrag(): void {
     this.origin = { x: this.position.x1, y: this.position.y1 };
+    this.origin2 = { x: this.position.x2, y: this.position.y2 };
   }
 }
